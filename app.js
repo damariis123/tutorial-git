ddfac52074b1e4e017cca33741e9d4f0eecf1939
Ramirojoaquin <yapurameline7@gmail.com>
2022-11-16 11:16:23 -0300

    Iniciomirepositorio

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log(document)
+document.querySelector('h1').textContent ='Texto desde JS'
+console.log(document.querySelector('h1'))
+
+document.querySelector('p')
+console.log(document.querySelector('h3'))
+document.querySelector('.h3-dos')
+console.log(document.querySelector('.h3-dos'))
+console.log(document.querySelector('#p-dos'))
+console.log(document.getElementById('p-dos'))
+console.log(document.querySelectorAll('.p-uno'))
